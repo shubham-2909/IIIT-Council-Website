70a4b47e6ec054c8c1921a02af0e9f0e8a8ef364
sg976 <sg9769356@gmail.com>
2022-09-08 15:49:59 +0530

    firsr commit

--- app.js
+++ app.js
@@ -7,6 +7,30 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.get('/', async (req,res)=>{
     res.sendFile(__dirname + "/index.html")
 })
+app.get('/minutes', async (req,res)=>{
+    res.sendFile(__dirname + "/minutes.html")
+})
+app.get('/member_inst', async(req, res)=>{
+    res.sendFile(__dirname + "/member_inst.html")
+})
+app.get('/admissons', async(req, res)=>{
+    res.sendFile(__dirname + "/admissons.html")
+})
+app.get('/rules', async(req, res)=>{
+    res.sendFile(__dirname + "/rules.html")
+})
+app.get('/acts', async(req, res)=>{
+    res.sendFile(__dirname + "/acts.html")
+})
+app.get('/structure', async(req, res)=>{
+    res.sendFile(__dirname + "/structure.html")
+})
+app.get('/students', async(req, res)=>{
+    res.sendFile(__dirname + "/students.html")
+})
+app.get("/academics", async(req,res)=>{
+    res.sendFile(__dirname + "/academics.html")
+})
 app.listen(port, async ()=>{
     console.log(`Server is Created on ${port}`);
 })
